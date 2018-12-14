b1070dc059e505068875215fca33c15543eb488e
yasuhara <h16s4057@hirosaki-u.ac.jp>
2018-12-14 22:18:03 +0900

    image list view dekita o-dori

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080025;
-        public static final int end = 0x7f080043;
-        public static final int gone = 0x7f080052;
-        public static final int invisible = 0x7f08005f;
-        public static final int left = 0x7f080064;
-        public static final int packed = 0x7f080082;
-        public static final int parent = 0x7f080084;
-        public static final int percent = 0x7f080087;
-        public static final int right = 0x7f08008c;
-        public static final int spread = 0x7f0800b0;
-        public static final int spread_inside = 0x7f0800b1;
-        public static final int start = 0x7f0800b6;
-        public static final int top = 0x7f0800cf;
-        public static final int wrap = 0x7f0800df;
+        public static final int end = 0x7f080044;
+        public static final int gone = 0x7f080054;
+        public static final int invisible = 0x7f080060;
+        public static final int left = 0x7f080065;
+        public static final int packed = 0x7f080084;
+        public static final int parent = 0x7f080086;
+        public static final int percent = 0x7f080089;
+        public static final int right = 0x7f08008e;
+        public static final int spread = 0x7f0800b2;
+        public static final int spread_inside = 0x7f0800b3;
+        public static final int start = 0x7f0800b8;
+        public static final int top = 0x7f0800d1;
+        public static final int wrap = 0x7f0800e1;
     }
     public static final class styleable {
         private styleable() {}
--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/maps/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/maps/R.java
@@ -39,11 +39,11 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int hybrid = 0x7f080057;
-        public static final int none = 0x7f08007c;
-        public static final int normal = 0x7f08007d;
-        public static final int satellite = 0x7f08008f;
-        public static final int terrain = 0x7f0800bf;
+        public static final int hybrid = 0x7f080059;
+        public static final int none = 0x7f08007e;
+        public static final int normal = 0x7f08007f;
+        public static final int satellite = 0x7f080091;
+        public static final int terrain = 0x7f0800c1;
     }
     public static final class styleable {
         private styleable() {}
