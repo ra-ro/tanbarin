28ae0f9e52a05798cfe725cfe91a2dfacb7ae931
R.Yama <pensukexp@gmail.com>
2019-01-25 23:47:23 +0900

    プッシュ通知&位置情報途中
    鯖との連携はうまくいってるんだが、配信されたものが上手く表示されぬ

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/design/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/design/R.java
@@ -1285,17 +1285,17 @@ public final class R {
         public static final int bottom_sheet_behavior = 0x7f0e002b;
         public static final int character_counter_content_description = 0x7f0e002f;
         public static final int character_counter_pattern = 0x7f0e0030;
-        public static final int fab_transformation_scrim_behavior = 0x7f0e0044;
-        public static final int fab_transformation_sheet_behavior = 0x7f0e0045;
-        public static final int hide_bottom_view_on_scroll_behavior = 0x7f0e0049;
-        public static final int mtrl_chip_close_icon_content_description = 0x7f0e004b;
-        public static final int password_toggle_content_description = 0x7f0e004c;
-        public static final int path_password_eye = 0x7f0e004d;
-        public static final int path_password_eye_mask_strike_through = 0x7f0e004e;
-        public static final int path_password_eye_mask_visible = 0x7f0e004f;
-        public static final int path_password_strike_through = 0x7f0e0050;
-        public static final int search_menu_title = 0x7f0e0053;
-        public static final int status_bar_notification_info_overflow = 0x7f0e0054;
+        public static final int fab_transformation_scrim_behavior = 0x7f0e0045;
+        public static final int fab_transformation_sheet_behavior = 0x7f0e0046;
+        public static final int hide_bottom_view_on_scroll_behavior = 0x7f0e0051;
+        public static final int mtrl_chip_close_icon_content_description = 0x7f0e0053;
+        public static final int password_toggle_content_description = 0x7f0e0054;
+        public static final int path_password_eye = 0x7f0e0055;
+        public static final int path_password_eye_mask_strike_through = 0x7f0e0056;
+        public static final int path_password_eye_mask_visible = 0x7f0e0057;
+        public static final int path_password_strike_through = 0x7f0e0058;
+        public static final int search_menu_title = 0x7f0e005c;
+        public static final int status_bar_notification_info_overflow = 0x7f0e005d;
     }
     public static final class style {
         private style() {}
